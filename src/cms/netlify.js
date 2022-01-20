b92f33509ea1314e862f812a9dd5bfb8e31c3c7a
Iván Campaña <ivan.campana@gmail.com>
2022-01-20 10:24:48 -0500

    Updated netlify cms config.

--- src/cms/netlify.js
+++ src/cms/netlify.js
@@ -1,4 +1,6 @@
 import CMS from 'netlify-cms-app';
 import { TagControl, TagPreview } from '../components/tagWidget';
 
+import '../css/layout.css';
+
 CMS.registerWidget('tags', TagControl, TagPreview);
